a4cfeeb2275be0003cf8c45ce6a2738495799bf8
Vedant_1 <vedantande.45@gmail.com>
2025-06-12 11:31:35 +0530

    Setup folders for controllers and routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.ts';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.ts";
+import studentRoutes from "./routes/studentRoutes.ts";
 
 dotenv.config();
 
@@ -11,9 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('PES Backend is running');
+app.get("/", (_req: Request, res: Response) => {
+  res.send("PES Backend is running");
 });
+
+// Routes
+app.use("/api/student", studentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
