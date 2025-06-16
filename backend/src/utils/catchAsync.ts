63ed0d0ba0c8fbbd88595d912935c7e4835364f1
PodaMounitha <176000994+PodaMounitha@users.noreply.github.com>
2025-06-16 09:27:28 +0530

    Create catchAsync.ts

---
+++ backend/src/utils/catchAsync.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+type AsyncFn = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+
+const catchAsync = (fn: AsyncFn) => (req: Request, res: Response, next: NextFunction) => {
+  fn(req, res, next).catch(next);
+};
+
+export default catchAsync;
